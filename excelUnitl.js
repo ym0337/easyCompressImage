8807f0a9cace772ab42bd55c56c7ad6caaad0307
姚旭明 <yaoxuming@csg.cn>
2024-08-22 16:48:38 +0800

    fix:过滤置灰菜单

--- excelUnitl.js
+++ excelUnitl.js
@@ -3,7 +3,7 @@
 const xlsx = require('xlsx');
 const path = require('path');
 const fs = require('fs');
-const {writeFile} = require('./uitls/uitils.js')
+const { writeFile } = require('./uitls/uitils.js')
 
 const fileName = '菜单调整V1-20240726.xlsx'
 
@@ -20,10 +20,13 @@ const sheetName = workbook.SheetNames[1];
 const worksheet = workbook.Sheets[sheetName];
 
 // 将工作表内容转换为JSON格式
-const data = xlsx.utils.sheet_to_json(worksheet);
-if (data) {
+const _data = xlsx.utils.sheet_to_json(worksheet);
+let data = []
+if (_data) {
   // data.shift(); // 删除第一列表头
-  // console.log(data,'data')
+  // 移除置灰的数据
+  data = _data.filter(item => item.isActive != '置灰')
+
 }
 
 // 输出读取的数据(这个表格有用的数据范围)
@@ -33,11 +36,11 @@ if (data) {
 // 创建一级节点
 const dom1 = createDom('业务域', data);
 // 创建二级节点
-const dom2 = createDom('一级菜单', data);
+const dom2 = createDom('一级菜单', data, '业务域');
 // 创建三级节点
-const dom3 = createDom('二级菜单', data);
+const dom3 = createDom('二级菜单', data, '一级菜单');
 // 创建四级节点
-const dom4 = createDom('三级菜单', data, 'id');
+const dom4 = createDom('三级菜单', data, 'uuid');
 
 const DomObj = [];
 
@@ -62,13 +65,12 @@ dom2.forEach(item => {
         '业务域': item['业务域'],
         '一级菜单': item['一级菜单'],
         name: item['一级菜单'],
-        defaultIcon: item['defaultIcon业务域'],
-        activeIcon: item['activeIcon业务域'],
+        defaultIcon: item['defaultIcon一级菜单'],
+        activeIcon: item['activeIcon一级菜单'],
         permissionCode: item['permissionCode一级菜单'],
         list: [],
       })
     }
-
   })
 });
 
@@ -100,27 +102,29 @@ dom4.forEach(item => {
     item2.list.forEach(item3 => {
       item3.list.forEach(item4 => {
         if (item2.name === item['业务域'] && item3.name === item['一级菜单'] && item4.name === item['二级菜单']) {
-          item4.list.push({
-            ...item,
-            name: item['三级菜单'],
-            list: null
-          })
+            item4.list.push({
+              ...item,
+              name: item['三级菜单'],
+              list: null
+            })
         }
       })
     })
   })
 });
 
+
+
 // JSON.stringify()方法用于将JavaScript对象转换为JSON字符串，第二个参数null表示不替换任何值，第三个参数2表示格式化输出，每两个层级缩进两个空格。最后，使用export default将转换后的字符串作为模块的默认输出。
 const content = `export default ${JSON.stringify(DomObj, null, 2)}`;
 const filePathOut = path.join(__dirname, '/resoure', 'menus.js');
 writeFile(filePathOut, content)
 
-function createDom (type, data, id='') {
+function createDom (type, data, uuid = '') {
   const seen = new Set();
   const uniqueArray = [];
   data.filter(item => {
-    const keyValue = item[id] || item[type];
+    const keyValue = uuid ? item[type] + item[uuid] : item[type];
     if (seen.has(keyValue)) {
       return false;
     } else {
